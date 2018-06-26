cf9f51b94c018865509da0866363b74e8ac79e3f
duongnhn96 <37268222+duongnhn96@users.noreply.github.com>
2018-06-26 22:29:35 +0700

    a

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -5,6 +5,7 @@ import { MENU_ITEMS } from './pages-menu';
 @Component({
   selector: 'ngx-pages',
   template: `
+  
     <ngx-sample-layout>
       <nb-menu [items]="menu"></nb-menu>
       <router-outlet></router-outlet>
